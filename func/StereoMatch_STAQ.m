82fb0b99cb9e5426bcb35aefe9c6ca7281d12f55
Zach Tong_Home_PC <zachtong@utexas.edu>
2026-04-23 01:51:31 -0500

    refactor(stereo): remove noisy per-call FFT summary print

--- func/StereoMatch_STAQ.m
+++ func/StereoMatch_STAQ.m
@@ -5,7 +5,8 @@ function [StereoInfo, RD_L, RD_R] = StereoMatch_STAQ(RD_L,RD_R,Normalized_L,Norm
 %
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-DICpara.showImgOrNot = 1;
+% Respect caller's showImgOrNot setting (don't override to 1)
+if ~isfield(DICpara,'showImgOrNot'), DICpara.showImgOrNot = 1; end
 incOrNot = DICpara.DICIncOrNot; % 0: acc, 1: inc
 Df = funImgGradient(Normalized_L,Normalized_L,NormalizedMask_L); % Finite difference to compute image grayscale gradients;
 
@@ -18,20 +19,30 @@ Df = funImgGradient(Normalized_L,Normalized_L,NormalizedMask_L); % Finite differ
 % ====== Initial Value ======
 U0 = Init(u,v,cc.max,DICmesh.x0,DICmesh.y0,0);
 
-% Zach Modified
-% Set zero at holes
-linearIndices1 = sub2ind(size(NormalizedMask_L), DICmesh.coordinatesFEM(:,1), DICmesh.coordinatesFEM(:,2));
+% Set NaN at holes using mask
+linearIndices1 = sub2ind(size(NormalizedMask_L), round(DICmesh.coordinatesFEM(:,1)), round(DICmesh.coordinatesFEM(:,2)));
 MaskOrNot1 = NormalizedMask_L(linearIndices1);
-% Do we need to consider the mask of g? TBD
-% u_inv = u'; v_inv = v';
-% linearIndices2 = sub2ind(size(NormalizedMask_R), floor(DICmesh.coordinatesFEM(:,1)+u_inv(:)), floor(DICmesh.coordinatesFEM(:,2)+v_inv(:)));
-% MaskOrNot2 = NormalizedMask_R(linearIndices2);
-% MaskOrNot = MaskOrNot1 + MaskOrNot2;
-% nanIndex = find(MaskOrNot<2);
 
 nanIndex = find(MaskOrNot1<1);
 U0(2*nanIndex) = nan;
 U0(2*nanIndex-1) = nan;
+
+nTotal = size(DICmesh.coordinatesFEM,1);
+nMasked = length(nanIndex);
+nValid = nTotal - nMasked;
+fprintf('  Mask: %d/%d valid (%.0f%%)\n', nValid, nTotal, 100*nValid/nTotal);
+
+if nValid < 10
+    warning('StereoMatch: only %d valid points after masking! Check mask orientation.', nValid);
+    % Debug: sample mask values at a few coordinate locations
+    sampleIdx = round(linspace(1, nTotal, min(10,nTotal)));
+    for si = 1:length(sampleIdx)
+        idx = sampleIdx(si);
+        cx = round(DICmesh.coordinatesFEM(idx,1));
+        cy = round(DICmesh.coordinatesFEM(idx,2));
+        fprintf('  coord(%d)=[%d,%d] -> mask=%d\n', idx, cx, cy, NormalizedMask_L(cx,cy));
+    end
+end
 
 % ====== Deal with incremental mode ======
 % RD_L.ResultFEMesh{1+floor(1/DICpara.ImgSeqIncUnit)} = ... % To save first mesh info
@@ -48,6 +59,8 @@ DICmesh.elementMinSize = DICpara.winsizeMin; % min element size in the refined q
 % Hanging nodes and sub-elements are placed on the last
 % All the void regions are generating nodes but we can ignore them
 % using maskfile later.
+% Quadtree mesh generation
+
 [DICmesh,DICpara,U0] = GenerateQuadtreeMesh(U0,Df,NormalizedMask_L,DICmesh,DICpara); % Generate the quadtree mesh
 
 % ====== Store current mesh ======
@@ -56,6 +69,10 @@ StereoInfo.ResultFEMeshEachFrame = struct( 'coordinatesFEM',...
     'markCoordHoleEdge',DICmesh.markCoordHoleEdge );
 
 %% ICGN
+nQuadPts = size(DICmesh.coordinatesFEM, 1);
+nQuadNaN = nnz(isnan(U0(1:2:end)));
+fprintf('  Quadtree: %d pts, %.0f%% valid\n', nQuadPts, 100*(nQuadPts-nQuadNaN)/nQuadPts);
+
 tol = 1e-3;
 
 if stereoMatchShapeOrder == 1
@@ -68,9 +85,10 @@ else
         Normalized_L,Normalized_R,DICpara,'GaussNewton',tol,2);
 end
 
-% ------ Plot ------
-close all; % Plotuv(U,DICmesh.x0,DICmesh.y0);
-Plotdisp_show(U,DICmesh.coordinatesFEM,DICmesh.elementsFEM);
+% Plot (only if showImgOrNot=1)
+if nnz(~isnan(U)) > 0 && DICpara.showImgOrNot == 1
+    Plotdisp_show(U,DICmesh.coordinatesFEM,DICmesh.elementsFEM);
+end
 %Plotdisp_show(U0,DICmesh.coordinatesFEM,DICmesh.elementsFEM);
 
 
@@ -84,8 +102,16 @@ StereoInfo.U=U;
 
 
 % Determine the corresponding ROI in the first right image
-Utemp = reshape(StereoInfo.U,2,size(StereoInfo.U,1)/2)';
-StereoInfo.ResultFEMesh_corr = StereoInfo.ResultFEMeshEachFrame.coordinatesFEM + Utemp;  % If strategy 2, this variable could be modify as cells
+nCoords = size(StereoInfo.ResultFEMeshEachFrame.coordinatesFEM, 1);
+if numel(StereoInfo.U) == 2*nCoords
+    Utemp = reshape(StereoInfo.U, 2, nCoords)';
+    StereoInfo.ResultFEMesh_corr = StereoInfo.ResultFEMeshEachFrame.coordinatesFEM + Utemp;
+else
+    warning('StereoMatch: U size (%d) != 2*coordinatesFEM (%d). Using zero disparity.', ...
+        numel(StereoInfo.U), 2*nCoords);
+    Utemp = zeros(nCoords, 2);
+    StereoInfo.ResultFEMesh_corr = StereoInfo.ResultFEMeshEachFrame.coordinatesFEM + Utemp;
+end
 RD_R.Coordinates_corr = StereoInfo.ResultFEMesh_corr;
 
 StereoInfo.CalPointsNum = size(u);
