f55efa6ab38b2283250a16dcd90cf4a846396203
Zach Tong_Home_PC <zachtong@utexas.edu>
2026-04-23 02:29:11 -0500

    refactor(main): remove dead code, Challenge 2.1 leaks, typos (C1a)
    - Drop commented ImgRefMask_left/right duplicates in Section 2
    - Drop 'Zach comments' block in Section 3.1; rewrite as neutral comment
    - Drop debug mask figures commented in Section 3.2
    - Drop embedded 'Test 3D construction' block (stereo-reconstruction
      sanity check inline in production flow); replaced with conditional
      call to verify_stereo_calibration() helper (to be added in C3.5)
    - Drop typo variable 'shapeFuncOalized_rder' in Section 4 (never used)
    - Fix Section banner: 'Section 8 Start/Done' -> 'Section 6 Start/Done'
    - Drop 'Choose strain computation method' commented prompts (unused)
    - Remove Challenge 2.1 Bespoke hardcoded base-points override that
      clobbered the interactive getBasePoints() result (private data leak)
    - Drop commented PlotWarpage / Plotdisp_show_3D / SaveFigFiles blocks
    - Drop commented save() cleanup at end of script
    - Replace hardcoded '\' path separator with fullfile() for portability
    - Simplify inner try/catch smoothing block (no branch change)
    
    Net result: 403 -> 314 lines (-22%). No algorithm change.

--- main_3D_ALDIC.m
+++ main_3D_ALDIC.m
@@ -112,27 +112,16 @@ end
 
 DICpara.ImgRefMask = double(maskLeft{1});
 
-
-% DICpara.ImgRefMask_left = double(maskLeft{1});
-% DICpara.ImgRefMask_right = double(maskRight{1});
-
-
 fprintf('------------ Section 2 Done ------------ \n \n')
 
 %% Section 3.1 Stereo Calibration
 calib_method = funParaInput('CalibrationMethod');
-% calib_method = 0;
 switch calib_method
     case 0
-        % Calibration in MATLAB
-        % ----------  Zach comments  --------------------
-        % Do not use this line, please install CV toolbox and use stereo 
-        % camera calibrator app to calibrate your cameras.
-        % StereoInfo = StereoCameraCalibration; 
-        % ---------------------------------------
-        
-        % Export cal. parameters from Matlab calibrator in the CV toolbox
-        StereoInfo = cameraParamsFormatConvertFromMatlabCV; 
+        % Install Computer Vision Toolbox and use the Stereo Camera
+        % Calibrator app to calibrate your cameras; then export via
+        % cameraParamsFormatConvertFromMatlabCV below.
+        StereoInfo = cameraParamsFormatConvertFromMatlabCV;
 
     case 1
         % Import Calib. results from MatchID Calibrator
@@ -161,54 +150,20 @@ stereoMatchShapeOrder = 1; % Currently, we only support 1st shape function
 [StereoInfo, RD_L, RD_R] = StereoMatch_STAQ(RD_L,RD_R,imgNormalized_L{1},imgNormalized_R{1},...
     fileNameLeft,maskLeft{1},maskRight{1} ,DICpara,StereoInfo,stereoMatchShapeOrder);
 
-% debug
-% figure; imshow(maskLeft{1});
-% figure; imshow(maskRight{1});
-
-%%%%%%%%%%%%%%%%%%%%% Test 3D construction: START %%%%%%%%%%%%%%%%%%%%%
-RD0_L_Pts = StereoInfo.ResultFEMeshEachFrame.coordinatesFEM;
-RD0_R_Pts = StereoInfo.ResultFEMesh_corr;
-figure, plot(RD0_L_Pts(:,1),RD0_L_Pts(:,2),'o')
-hold on; plot(RD0_R_Pts(:,1),RD0_R_Pts(:,2),'o')
-
-matchedPairs{1,1} = [RD0_L_Pts, RD0_R_Pts];
-
-% Try to calculate the reprojection errors using the first two frames to reconstruct 3D coordinates
-% [FinalResult.Coordinates,reprojectionErrors] = stereoReconstruction(StereoInfo.cameraParams, RD0_L_Pts, RD0_R_Pts);
-
-cameraParams = StereoInfo.cameraParams;
-%cameraParams = StereoInfo.cameraParams_optimized;
-
-K_left = cameraParams.cameraParamsLeft.K;
-K_right = cameraParams.cameraParamsRight.K;
-R_left = [1 0 0; 0 1 0; 0 0 1]; % Set left Camera Coordinate as World Coordinate
-T_left = [0 0 0]'; % Set left Camera Coordinate as World Coordinate
-R_right = cameraParams.rotationMatrix;
-T_right = cameraParams.translationVector';
-
-% Undistort points before doing 3D reconstruction
-[matchedPairs_undistort]= funUndistortPoints(matchedPairs,cameraParams);
-
-P_left = K_left * [R_left, T_left];
-P_right = K_right * [R_right, T_right];
-reconstructedPoints = cell(size(matchedPairs_undistort,1),1);
-reprojectionErrors = cell(size(matchedPairs_undistort,1),1);
-
-for i = 1:size(matchedPairs_undistort,1)
-    [reconstructedPoints{i,1},reprojectionErrors{i}]= triangulate(matchedPairs_undistort{i,1}(:, 1:2), matchedPairs_undistort{i,1}(:, 3:4), P_left, P_right);
+% Optional: sanity-check stereo calibration on frame 1. Extract to its
+% own helper (verify_stereo_calibration.m) in the C3.5 workstream.
+if isfield(DICpara, 'verifyStereoReconstruction') && DICpara.verifyStereoReconstruction
+    verify_stereo_calibration(StereoInfo);
 end
-figure; scatter3(reconstructedPoints{1,1}(:,1),reconstructedPoints{1,1}(:,2),reconstructedPoints{1,1}(:,3));
-disp(['mean_repo = ',num2str(mean(reprojectionErrors{1}))]);
-% [reconstructedPoints,reprojectionErrors] = triangulatePoints(matchedPairs_undistort, K_left, K_right, R_left, T_left, R_right, T_right);
-%%%%%%%%%%%%%%%%%%%%% Test end %%%%%%%%%%%%%%%%%%%%%
 
 %% Section 4 Temporal Matching
+% Only 1st-order shape function is implemented; 2nd-order path not supported.
+shapeFuncOrder = 1;
+
 % Left image series
-shapeFuncOrder = 1; % Curre1ntly, we only support 1st shape function
 RD_L = TemporalMatch_quadtree_ST1(DICpara, fileNameLeft,maskLeft,imgNormalized_L,RD_L,StereoInfo, 'camera0',shapeFuncOrder);
 
 %% Right image series
-shapeFuncOalized_rder = 1; % Curre1ntly, we only support 1st shape function
 RD_R = TemporalMatch_quadtree_ST1(DICpara,fileNameRight,maskRight,imgNormalized_R,RD_R,StereoInfo, 'notCamera0',shapeFuncOrder);
 
 %% Section 5 3D-Result
@@ -222,39 +177,31 @@ check3DReconstructionResults(reprojectionErrors, FinalResult, RD_L,13);
 
 %% Section 6: Compute strains/ Plot disp. and strains
 close all;
-fprintf('------------ Section 8 Start ------------ \n')
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% This section is to compute strain fields and plot disp and strain results
-% %%%%%%%%%%%%%%%%%%%%5%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fprintf('------------ Section 6 Start ------------ \n')
+% -------------------------------------------------------------------
+% This section computes strain fields and plots disp and strain results.
+% -------------------------------------------------------------------
 % ------ Convert units from pixels to the physical world ------
-% DICpara.um2px = funParaInput('ConvertUnit');
 DICpara.um2px = 1;
 
 % Image save path
 DICpara.outputFilePath = [];
 
 % ------ Smooth displacements ------
-%DICpara.DoYouWantToSmoothOnceMore = funParaInput('SmoothDispOrNot');
 DICpara.DoYouWantToSmoothOnceMore = 0;
 
-% ------ Choose strain computation method ------
-%DICpara.MethodToComputeStrain = funParaInput('StrainMethodOp');
-
-% ------ Choose strain type (infinitesimal, Eulerian, Green-Lagrangian) ------
-%DICpara.StrainType = funParaInput('StrainType');
-DICpara.StrainType = 0; % Currently, only support Green-Lagrangian strain
+% ------ Strain type (only Green-Lagrangian is supported) ------
+DICpara.StrainType = 0;
 
 % ------ Choose image to plot (first only, second and next images) ------
 DICpara.Image2PlotResults = funParaInput('Image2PlotResults');
 
 % ------ Save fig format ------
 DICpara.MethodToSaveFig = funParaInput('SaveFigFormat');
-%DICpara.MethodToSaveFig = 1;
 
 % ------ Choose overlay image transparency ------
 DICpara.OrigDICImgTransparency = 1;
 if DICpara.MethodToSaveFig == 1
-    % DICpara.OrigDICImgTransparency = funParaInput('OrigDICImgTransparency');
     DICpara.OrigDICImgTransparency = 0.8;
 end
 
@@ -269,7 +216,7 @@ if DICpara.transformDisp == 1
 end
 
 
-% ------ Start main part ------
+% ------ Strain window size ------
 prompt = 'What is your strain size? e.g. 3,5,7...\nInput: ';
 strain_size = input(prompt);
 strain_length = (strain_size-1) * DICpara.winstepsize + 1;
@@ -283,30 +230,19 @@ FinalResult.ResultStrainWorld{1} = 0;
 FinalResult.Displacement_smooth(1,:) = FinalResult.Displacement(1,:); % All zeros
 
 for ImgSeqNum = 2: length(imgNormalized_L)
-    disp(['Current image frame #: ', num2str(ImgSeqNum),'/',num2str(length(imgNormalized_L))]);
+    fprintf('  Frame %d/%d\n', ImgSeqNum, length(imgNormalized_L));
     close all;
     ImageName = fileNameLeft{1,ImgSeqNum};
-    FullImageName_current = [fileNameLeft{2,ImgSeqNum} ,'\', fileNameLeft{1,ImgSeqNum}];
-    FullImageName_first = [fileNameLeft{2,1} ,'\', fileNameLeft{1,1}];
+    FullImageName_current = fullfile(fileNameLeft{2,ImgSeqNum}, fileNameLeft{1,ImgSeqNum});
+    FullImageName_first   = fullfile(fileNameLeft{2,1}, fileNameLeft{1,1});
 
-    % x0World = DICpara.um2px*x0;
-    % y0World = DICpara.um2px*y0; % Ignore this: (size(ImgNormalized_L{1},2)+1-y0);
-
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % ------ Smooth displacements ------
-    % prompt = 'Do you want to smooth displacement? (0-yes; 1-no)';
-    % DoYouWantToSmoothOnceMore = input(prompt);
-    % ------ Smooth displacements ------
-
+    % ------ Smooth displacements (post-ALDIC, 3D) ------
     SmoothTimes = 0;
     try
         while DICpara.DoYouWantToSmoothOnceMore == 0 && SmoothTimes < 3
-            % if DICpara.transformDisp == 0
-            %     FinalResult.Displacement_smooth(ImgSeqNum,:) = funSmoothDisp_Quadtree(FinalResult.DisplacementNew(ImgSeqNum,:),RD_L.ResultFEMeshEachFrame{1},DICpara);
-            % else
-            FinalResult.Displacement_smooth(ImgSeqNum,:) = funSmoothDisp_Quadtree(FinalResult.Displacement(ImgSeqNum,:),RD_L.ResultFEMeshEachFrame{1},DICpara);
-            % end
-            %%DICpara.DoYouWantToSmoothOnceMore = input(prompt);
+            FinalResult.Displacement_smooth(ImgSeqNum,:) = funSmoothDisp_Quadtree( ...
+                FinalResult.Displacement(ImgSeqNum,:), ...
+                RD_L.ResultFEMeshEachFrame{1}, DICpara);
             SmoothTimes = SmoothTimes + 1;
         end
     catch
@@ -321,21 +257,16 @@ for ImgSeqNum = 2: length(imgNormalized_L)
         FinalResult.Coordinates(ImgSeqNum,:), FinalResult.Displacement_smooth(ImgSeqNum,:),...
         FullImageName_first, ImgSeqNum, 'Local'); % 'Local' or 'Specific Coordinate'
 
-    % Pre-display displacement results
-    % close all; Plotdisp_show_3D(FinalResult.Displacement(ImgSeqNum,:),RD_L.ResultFEMeshEachFrame{1,1}.coordinatesFEM,...
-    %     RD_L.ResultFEMeshEachFrame{1,1}.elementsFEM,DICpara,'NoEdgeColor');
-
-
     % ---------------------------------------------------------
     % Plotting Configuration - Easy Mode Switching
     % ---------------------------------------------------------
 
     % Define what to plot
     DICpara.plots_disp_to_generate = {'u', 'v', 'w', 'magnitude'};
-    DICpara.plots_strain_to_generate = {'exx', 'eyy', 'exy'};
+    DICpara.plots_strain_to_generate = {'exx', 'eyy', 'exy', 'e1', 'e2'};
     % === QUICK MODE SELECTION ===
     % Choose one of: 'auto', 'custom', 'interactive'
-    plotting_mode = 'interactive';  % Change this line to switch modes instantly
+    plotting_mode = 'auto';  % Change this line to switch modes instantly
 
     DICpara = setPlottingParameters(DICpara, plotting_mode);
 
@@ -347,11 +278,6 @@ for ImgSeqNum = 2: length(imgNormalized_L)
         if DICpara.transformDisp == 0
             PlotdispQuadtreeMasks3D_acc_ST1(FinalResult.DisplacementNew(ImgSeqNum,:),RD_L.ResultDisp{ImgSeqNum-1,1}.U,...
                 RD_L.ResultFEMeshEachFrame{1,1}, FullImageName_first,FullImageName_current,DICpara,voidIndex);
-            
-            % Zach Optional：
-            % PlotWarpage(FinalResult.warpage(ImgSeqNum,:),RD_L.ResultDisp{ImgSeqNum-1,1}.U,...
-            %     RD_L.ResultFEMeshEachFrame{1,1}, FullImageName_first,FullImageName_current,DICpara,voidIndex);
-   
         else
             PlotdispQuadtreeMasks3D_acc_ST1(FinalResult.Displacement(ImgSeqNum,:),RD_L.ResultDisp{ImgSeqNum-1,1}.U,...
                 RD_L.ResultFEMeshEachFrame{1,1}, FullImageName_first,FullImageName_current,DICpara,voidIndex);
@@ -381,19 +307,8 @@ for ImgSeqNum = 2: length(imgNormalized_L)
         'strain_eyy',strain_eyy,'strain_exy',strain_exy,'strain_principal_max',strain_principal_max,'strain_principal_min',strain_principal_min, ...
         'strain_maxshear',strain_maxshear,'strain_vonMises',strain_vonMises);
     clear strain_exx  strain_eyy  strain_ezz  strain_exy  strain_eyz  strain_exz strain_maxshear strain_principal_max strain_principal_min strain_vonMises
-   
-    % ------ Save figures for tracked displacement and strain fields ------
-    SaveFigFilesDispAndStrain(DICpara, fileNameLeft, ImgSeqNum,'FileFormat',DICpara.MethodToSaveFig);
     close all;
 
 end
 
-% ------ END of for-loop {ImgSeqNum = 2:length(ImgNormalized_L)} ------
-fprintf('------------ Section 8 Done ------------ \n \n')
-
-% ------ Save data again including solved strain fields ------
-% selectedFolder = uigetdir('', 'Select save folder');
-% results_name = [selectedFolder,'\','results_',ImageName,'_ws',num2str(DICpara.winsize),'_st',num2str(DICpara.winstepsize),'.mat'];
-% save(results_name, 'FullImageName_current','DICpara','FinalResult');
-
-
+fprintf('------------ Section 6 Done ------------ \n \n')
